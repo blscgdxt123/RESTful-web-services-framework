1dbce6dba0dd455c695c9e7f87be667ed7b754e1
blscgdxt123 <118156228+blscgdxt123@users.noreply.github.com>
2022-12-15 15:14:56 +0800

    Add files via upload

---
+++ server/templates/collection.js
@@ -0,0 +1,48 @@
+{
+    "collection" :
+    {
+        "version" : "1.0",
+        "href" : "{@host}/api/",
+        "links" : [
+          {
+            "rel": "home",
+            "href": "http://localhost:1337"
+          }
+        ],
+        
+        "items" : 
+        {@list}
+        ,
+        {
+          "template" : {
+              "data": [
+                      {
+                        "name": "name",
+                        "value": "",
+                        "prompt": "name"
+                      },
+                      {
+                          "name": "studentid",
+                          "value": "",
+                          "prompt": "student id"
+                      },
+                      {
+                        "name": "email",
+                        "value": "",
+                        "prompt": "email"
+                      },
+                      {
+                          "name": "phone",
+                          "value": "",
+                          "prompt": "telephone number"
+                        },
+                      {
+                        "name": "hobby",
+                        "value": "",
+                        "prompt": "hobby"
+                      }
+                    ]
+              }
+      }
+}
+}
